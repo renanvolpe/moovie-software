667fe9b2ff915e6b73fe54038c96e57f85de68aa
Renan Pereira Volpe <renanvolpe067@gmail.com>
2025-07-08 11:27:55 -0300

    feat: start to make utils

---
+++ lib/utils/app_space.dart
@@ -0,0 +1,5 @@
+class AppSpace {
+  static const double btnWidthS = 100;
+  static const double btnWidthM = 150;
+  static const double btnWidthL = 200;
+}
---
+++ lib/utils/enums.dart
@@ -0,0 +1,1 @@
+enum BtnSize { free, s, m, l }
